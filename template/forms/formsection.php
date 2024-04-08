c3fd4970ecc0976ef2f746d7f5e827416307fa57
Vladlen Schetinin <vvschetinin@gmail.com>
2024-04-08 20:18:48 +0300

    Add Styles Send

--- template/forms/formsection.php
+++ template/forms/formsection.php
@@ -7,32 +7,37 @@
         <article class="cms-content">
           <h2 class="h2"><span>Ищете работу?</span></h2>
           <p>Отправьте нам свои данные, и мы свяжемся с вами</p>
-          <form method="post" class="vacancy-form">
-            <ul class="form-enter">
-              <li class="field-wrap">
-                <input type="text" name="name" placeholder="Ваше имя *" required>
-              </li>
-              <li class="field-wrap">
-                <input type="tel" name="phone" data-phone-pattern placeholder="Телефон *" required>
-              </li>
-              <li class="field-wrap">
-                <input type="email" name="email" placeholder="E-mail">
-              </li>
-              <li class="field-wrap">
-                <input type="text" name="vacancy" placeholder="Вакансия">
-              </li>
-              <li class="field-wrap">
-                <textarea name="message" rows="7" placeholder="Сообщение *" required></textarea>
-              </li>
-            </ul>
-            <div class="checkbox-container flex mt-1">
-              <input type="checkbox" name="formcheck" id="formcheck">
-              <label for="formcheck">Я даю согласие на обработку предоставленных данных</label>
+          <form action="#" id="main-form" method="post" class="vacancy-form">
+            <div class="form-body">
+              <div class="success"><span></span></div>
+              <ul class="form-enter">
+                <li class="field-wrap">
+                  <input type="text" name="name" placeholder="Ваше имя *" required>
+                </li>
+                <li class="field-wrap">
+                  <input type="tel" name="phone" data-phone-pattern placeholder="Телефон *" required>
+                </li>
+                <li class="field-wrap">
+                  <input type="email" name="email" placeholder="E-mail">
+                </li>
+                <li class="field-wrap">
+                  <input type="text" name="vacancy" placeholder="Вакансия">
+                </li>
+                <li class="field-wrap">
+                  <textarea name="message" rows="7" placeholder="Сообщение *" required></textarea>
+                </li>
+              </ul>
+              <div class="checkbox-container flex mt-1">
+                <input type="checkbox" name="formcheck" id="formcheck">
+                <label for="formcheck">Я даю согласие на обработку предоставленных данных</label>
+              </div>
             </div>
-            <div class="button-wrap mt-2">
+            <div class="form-footer button-wrap mt-2">
               <button class="btn btn-bg--red btn-form" id="formbutton" disabled>
                 <span class="btn-text">Отправить</span>&#10132;
               </button>
+              <input type="hidden" class="form-hidden" name="project_name" value="Сайт вакансий ЗМК МАМИ">
+              <input type="hidden" class="form-hidden" name="form_subject" value="Сообщение с формы обратной связи">
             </div>
           </form>
         </article>
